62c2279193fb2bd4b3afb0bba9a26f41ea9a40cb
huaisha1224 <sam.hxq@gmail.com>
2022-01-17 14:02:57 +0800

    Update excel.py
    优化导出文档格式

--- excel.py
+++ excel.py
@@ -10,24 +10,28 @@ def save_to_csv(dealtime,number,vercode, amount):
     """
     写入内容到csv
     """
-    file_path = strftime("jd-assistant_%Y_%m.csv")  #按照当前系统时间创建文件
+    file_path = strftime("jd-assistant_%Y_%m.csv")  # 按照当前系统时间创建文件
+    dealtime = dealtime.split(' ')[0]   # 切割时间取年月日
+    number = number + '\t'
+    vercode = vercode + '\t'
 
     #判断文件是否存在
     if os.path.exists(file_path):
         data = pd.DataFrame({'下单时间':dealtime, '订单号':number, '验证码':vercode, '实付金额':amount}, index=[0])
-        data['订单号'] = data['订单号'].apply(str) + '\t'
-        data['验证码'] = data['验证码'].apply(str) + '\t'
+        # data['订单号'] = data['订单号'].apply(str) + '\t'
+        # data['验证码'] = data['验证码'].apply(str) + '\t'
         data.to_csv(file_path, index=None, mode='a', header=None)
         #print('追加')
     else:
         data = pd.DataFrame({'下单时间':dealtime, '订单号':number, '验证码':vercode, '实付金额':amount}, index=[0])
-        data['订单号'] = data['订单号'].apply(str) + '\t'
-        data['验证码'] = data['验证码'].apply(str) + '\t'
+        # data['订单号'] = data['订单号'].apply(str) + '\t'
+        # data['验证码'] = data['验证码'].apply(str) + '\t'
         data.to_csv(file_path, index=None)
         #print('新增')
 
 
 if __name__ == '__main__':
+    # time = '2021-09-07'
     time = '2021-09-07 08:22:05'
     number = '221697592258'
     vercode = '7102298241802'
