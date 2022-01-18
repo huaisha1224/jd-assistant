cce116d5efad2c59f6771c0e4a6cb62f7e5d061f
huaisha1224 <sam.hxq@gmail.com>
2022-01-18 14:11:48 +0800

    Update excel.py
    用CSV模块替代Pandas、减少打包exe后的包大小

--- excel.py
+++ excel.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-from pandas import DataFrame
-import pandas as pd
 from time import strftime
 import os
+import csv
 
 def save_to_csv(dealtime,number,vercode, amount):
     """
     写入内容到csv
     """
-    file_path = strftime("jd-assistant_%Y_%m.csv")  # 按照当前系统时间创建文件
     dealtime = dealtime.split(' ')[0]   # 切割时间取年月日
     number = number + '\t'
     vercode = vercode + '\t'
+    amount = amount[0]
+    data = (dealtime, number, vercode, amount)
+    file_path = strftime("jd-assistant_%Y_%m.csv")  # 按照当前系统时间创建文件
 
-    #判断文件是否存在
+    # 判断文件是否存在
     if os.path.exists(file_path):
-        data = pd.DataFrame({'下单时间':dealtime, '订单号':number, '验证码':vercode, '实付金额':amount}, index=[0])
-        # data['订单号'] = data['订单号'].apply(str) + '\t'
-        # data['验证码'] = data['验证码'].apply(str) + '\t'
-        data.to_csv(file_path, index=None, mode='a', header=None)
-        #print('追加')
+        # 处理数据并写入
+        with open(file_path, 'a+', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
     else:
-        data = pd.DataFrame({'下单时间':dealtime, '订单号':number, '验证码':vercode, '实付金额':amount}, index=[0])
-        # data['订单号'] = data['订单号'].apply(str) + '\t'
-        # data['验证码'] = data['验证码'].apply(str) + '\t'
-        data.to_csv(file_path, index=None)
-        #print('新增')
-
+        # 创建文件 处理数据并写入
+        header = ('下单时间', '订单号', '验证码', '实付金额')
+        with open(file_path, 'a+', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(header)
+        
+        with open(file_path, 'a+', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
 
 if __name__ == '__main__':
-    # time = '2021-09-07'
     time = '2021-09-07 08:22:05'
     number = '221697592258'
     vercode = '7102298241802'
-    amount = '248.00'
+    amount = ['248.00']
     save_to_csv(time, number, vercode, amount)
