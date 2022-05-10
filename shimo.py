2e9132728281b83bffa876ee724c2990606daedd
huaisha1224 <sam.hxq@gmail.com>
2022-05-10 08:43:23 +0800

    Update shimo.py

--- shimo.py
+++ shimo.py
@@ -3,20 +3,19 @@
 """
     提交数据到石墨文档
 """
-
 import requests
 import random
 
-def shimo(content):
+def shimo(content, username):
    """提交数据到石墨文档表单收集器
    """
    # 石墨文档提交数据格式
    data = {
-      "duration" : 55,  #时间
+      "duration" : 55,
       "formRev" : 1,
       "responseContent" : [
          {
-            "guid" : "xxxxx",   #石墨文档的guid
+            "guid" : "3LloZtJ7", #xrmD2uAq/3LloZtJ7
             "text" : {
                "content" : "xxxxxxxx"
             },
@@ -24,29 +23,27 @@ def shimo(content):
          }
       ],
       "userFinger" : "-1",
-      "userName" : "JD"
+      "userName" : "xxxxx"
    }
 
-   url = "https://shimo.im/api/newforms/forms/xpVxJQJjT3GJ8WhJ/submit"
+   url = "https://shimo.im/api/newforms/forms/xpVxJQJjT3GJ8WhJ/submit" #真实地址 
+   #url = "https://shimo.im/api/newforms/forms/pdGc9jGYtdxyTydt/submit"  #测试地址 
 
-   data['duration'] = random.randint(20,40) #生成随机时间
+   data['duration'] = random.randint(10,50)
    data['responseContent'][0]['text']['content'] = content
-   data['userName'] = "JD助手"
-   # print("Duration: ", data['duration'])
-   # print("userName: ",data['userName'])
-   # print("Content: ", data['responseContent'][0]['text']['content'])
-   # print(data)
-
+   data['userName'] = username
+   #data['userName'] = "JD助手"
    response = requests.post(url, json=data)  #用post方式提交数据
    if response.status_code == 200 or 204:
       #print(response.status_code)
-      return True
+      return "提交成功"
    else:
-      return False
+      return "提交失败"
 
 if __name__ == '__main__':
-   vercode = ['7102298241802','1369696304672','5557372871652']
-   for x in vercode:
-      shimo(x)
-      # ok = shimo(x)
-      # print(ok)
+   content = '7102298241802'
+   username = "怀沙"
+   shimo(content, username)
+   # for x in vercode:
+   #    shimo(x)
+   #    print(x)
